2e13ce2e7400f6b3229b522a00bf5a091b510361
LuisPerezRodriguez <luiscesarp1995@gmail.com>
2024-10-23 12:46:28 -0600

    Nav actualizado

--- assets/script.js
+++ assets/script.js
@@ -4,3 +4,10 @@ const mobileNav = document.getElementById('mobile-nav');
 toggleBtn.addEventListener('click', () => {
   mobileNav.classList.toggle('show');
 });
+function checkWindowSize() {
+  // Si el ancho de la ventana es mayor que 1000px, eliminar la clase 'show'
+  if (window.innerWidth >= 1000) {
+    mobileNav.classList.remove('show');
+  }
+}
+window.addEventListener('resize', checkWindowSize);
