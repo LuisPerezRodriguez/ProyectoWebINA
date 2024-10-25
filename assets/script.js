085b3b4662ec4bae114621792f2ba4c4e45d9d08
LuisPerezRodriguez <luiscesarp1995@gmail.com>
2024-10-25 13:23:13 -0600

    Actualizacion de footer nav form y navegacion en index y proyecto de vida

--- assets/script.js
+++ assets/script.js
@@ -1,9 +1,17 @@
 const toggleBtn = document.getElementById('sub-nav__toggle-btn');
 const mobileNav = document.getElementById('mobile-nav');
+const mobileNavAnchors = document.getElementsByClassName('mobile-nav__itm-a');
 
 toggleBtn.addEventListener('click', () => {
   mobileNav.classList.toggle('show');
 });
+
+Array.from(mobileNavAnchors).forEach((anchor) => {
+  anchor.addEventListener('click', () => {
+    document.getElementById('mobile-nav').classList.toggle('show');
+  });
+});
+
 function checkWindowSize() {
   // Si el ancho de la ventana es mayor que 1000px, eliminar la clase 'show'
   if (window.innerWidth >= 1000) {
